26c60e4b28582355ea73bfba2f7f93ccbd6e8149
Paul <drpaulsaleh@gmail.com>
2024-10-20 23:09:12 +0100

    copilot fixing script

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import bluepy3 as bluepy
+from bluepy3 import btle
 import sys
 import json
 import paho.mqtt.client as mqtt
@@ -13,26 +13,25 @@ blue   = 'a495bb60c5b14b44b5121370f02d74de'
 yellow = 'a495bb70c5b14b44b5121370f02d74de'
 pink   = 'a495bb80c5b14b44b5121370f02d74de'
 
-class ScanDelegate(bluepy.DefaultDelegate):
+class ScanDelegate(btle.DefaultDelegate):
     def __init__(self):
-        bluepy.DefaultDelegate.__init__(self)
-        
+        super().__init__()
+
     def handleDiscovery(self, dev, isNewDev, isNewData):
-        if dev.scanData:
-            for (adtype, desc, value) in dev.getScanData():
-                if value == green:  # Change this to the colour of your tilt
-                    tempf = float(dev.rawData[4:6].hex(), 16)  # Example, might need adjustment
-                    tiltSG = float(dev.rawData[6:8].hex(), 16) / 1000
-                    tiltTemp = int((tempf - 32) * 5.0 / 9.0)
-                    tiltColour = 'GREEN'
-                    tiltBeer = 'tdrn3iX9ucmNpbA6n'  # Change to an identifier of a particular brew
-                    data = {
-                        'brewId': tiltBeer,
-                        'temperature': tiltTemp,
-                        'gravity': tiltSG
-                    }
-                    jsonObj = json.dumps(data)
-                    client.publish("tilt_data", jsonObj)
+        if dev.getValueText(btle.ScanEntry.COMPLETE_LOCAL_NAME):
+            if dev.addr in [green]:  # Change this to the colour of your tilt
+                tempf = float(dev.rawData[4:6].hex(), 16)  # Example, might need adjustment
+                tiltSG = float(dev.rawData[6:8].hex(), 16) / 1000
+                tiltTemp = int((tempf - 32) * 5.0 / 9.0)
+                tiltColour = 'GREEN'
+                tiltBeer = 'tdrn3iX9ucmNpbA6n'  # Change to an identifier of a particular brew
+                data = {
+                    'brewId': tiltBeer,
+                    'temperature': tiltTemp,
+                    'gravity': tiltSG
+                }
+                jsonObj = json.dumps(data)
+                client.publish("tilt_data", jsonObj)
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -48,7 +47,7 @@ def main():
     client.connect("mqtt.example.com", 1883, 60)  # Replace with your MQTT broker address and port
     client.loop_start()
 
-    scanner = bluepy.Scanner().withDelegate(ScanDelegate())
+    scanner = btle.Scanner().withDelegate(ScanDelegate())
     scanner.scan(10.0)  # Scans for 10 seconds
 
     client.loop_stop()
